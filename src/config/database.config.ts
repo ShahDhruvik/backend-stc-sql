36c606f33b553ae75b1b5df60642767eda028127
ShahDhruvik <devep1210@gmail.com>
2024-02-06 12:28:48 +0530

    changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,19 +1,34 @@
-import { Sequelize, Dialect } from 'sequelize';
+import Seq, { Sequelize, Dialect, Options } from 'sequelize';
 import getEnv from './env.config';
-const database = getEnv('SQL_DATABASE')
-const username = getEnv('SQL_USERNAME')
-const password = getEnv('SQL_PASSWORD')
-const dialect = getEnv('SQL_DIALECT')
-const host = getEnv('SQL_HOST')
-const sequelize = new Sequelize(database, username, password, {
-  host: host,
-  dialect: dialect as Dialect
-})
+import { queryLogger } from '../helpers/logger';
+
+
+const sequelizeOpts: Options = {
+  host: getEnv('SQL_HOST'),
+  dialect: getEnv('SQL_DIALECT') as Dialect,
+  logging: q => queryLogger.info(q),
+  retry: {
+    match: [/Deadlock/i, Seq.ConnectionError], // Retry on connection errors
+    max: 3, // Maximum retry 3 times
+    backoffBase: 3000, // Initial backoff duration in ms. Default: 100,
+    backoffExponent: 1.5, // Exponent to increase backoff each try. Default: 1.1
+  },
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 60000,
+    idle: 10000,
+  },
+}
+
+
 const connectDb = async () => {
   try {
+    const sequelize = new Sequelize(getEnv('SQL_DATABASE'), getEnv('SQL_USERNAME'), getEnv('SQL_PASSWORD'), sequelizeOpts)
     await sequelize.authenticate()
     console.log('Database connected!');
   } catch (error) {
+    console.log(error)
     console.log(error.message);
   }
 };
--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,6 +1,6 @@
 import { createLogger, format } from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
-const { json } = format;
+import DailyRotateFile, { DailyRotateFileTransportOptions } from 'winston-daily-rotate-file';
+const { json, combine, timestamp, printf } = format;
 
 const serviceName = 'common';
 
@@ -15,18 +15,33 @@ const getCwd = (x?: string) => {
 
 const { cwd } = getCwd('');
 
-const dailyRotate: DailyRotateFile = new DailyRotateFile({
-  filename: 'error-%DATE%.log',
-  datePattern: 'DD-MM-YYYY',
-  dirname: cwd + `/${serviceName}/` + '/logs/',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d',
-});
+const dailyRotate: (type: 'normal' | 'query') => DailyRotateFile = type => {
+  const opts: DailyRotateFileTransportOptions = {
+    filename: 'error-%DATE%.log',
+    datePattern: 'DD-MM-YYYY',
+    dirname: cwd + `/${serviceName}/` + '/logs/',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+  };
+  if (type === 'query') {
+    opts['filename'] = type + '-%DATE%.log';
+  }
+
+  return new DailyRotateFile(opts);
+};
 
 const logger = createLogger({
   format: json(),
-  transports: [dailyRotate],
+  transports: [dailyRotate('normal')],
 });
 
 export default logger;
+const custom = printf(({ message, timestamp }) => {
+  return `${timestamp} ${message}`;
+});
+
+export const queryLogger = createLogger({
+  format: combine(timestamp(), custom),
+  transports: [dailyRotate('query')],
+});
